77787a6e14ff0a95f6c845dc97deff2e54f71cce
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-06-04 20:07:55 -0700

    readme

--- k8s/bin/_install_project_template.sh
+++ k8s/bin/_install_project_template.sh
@@ -14,8 +14,10 @@ echo   "BASE_DIR : $BASE_DIR"
 #Check if project config exists
 
 if [ ! -f ${BASE_DIR}/${project_config_file} ]; then
+    echo .
     echo "Project config file ${project_config_file} missing in current directory"
     echo "Please go through the readme file at  ${template_git_repo} to create it."
+    echo .
     exit 0
 fi
 
@@ -53,5 +55,16 @@ chmod +x ${BASE_DIR}/k8s/bin/*.sh
 # Call Bootstrap script
 ${BASE_DIR}/k8s/bin/prepare_config.sh
 
+mkdir -p ${BASE_DIR}/k8s/deploy
+
+
 # Obscure /k8s/runtime-config from git checkin
 grep -qxF '/k8s/runtime-config' ${BASE_DIR}/.gitignore || echo '/k8s/runtime-config' >> ${BASE_DIR}/.gitignore
+
+echo .
+echo Next steps 
+echo    - Create application.yaml file for your staging environment under ${BASE_DIR}/k8s/runtime-config
+echo    - Run ${BASE_DIR}/k8s/bin/create_k8s.sh
+echo    - Populate the config maps for your application in kubernetes namespace.
+echo    - Create a Google cloud build trigger for your application
+echo .
